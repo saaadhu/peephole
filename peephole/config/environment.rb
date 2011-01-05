24c8dcb619f972c93304a5a25db2f3d804beac09
saaadhu <senthil.thecoder@gmail.com>
2011-01-05 23:08:30 +0530

    Added missing Product_Name

--- peephole/config/environment.rb
+++ peephole/config/environment.rb
@@ -1,5 +1,7 @@
 # Load the rails application
+Product_Name = "Peephole"
 require File.expand_path('../application', __FILE__)
+
 
 # Initialize the rails application
 Peephole::Application.initialize!
